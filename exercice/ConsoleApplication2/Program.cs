98aeae3fa4f323a715d1eeb42051c38179c27c33
vportemer <vportemer@VPORTEMER17-DE>
2017-03-24 18:29:15 +0100

    pop

--- exercice/ConsoleApplication2/Program.cs
+++ exercice/ConsoleApplication2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-
+            
             Boite[] tabBoite = new Boite[3];
             Boite maBoite = tabBoite[1];
 
